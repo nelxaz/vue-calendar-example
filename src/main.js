e159c96890a0e7821f36a1fa477668a39a507d91
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2020-04-25 17:43:24 -0400

    Add setup to firebase and vue-composition api plugin

--- src/main.js
+++ src/main.js
@@ -1,27 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueComposition from '@vue/composition-api';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
-import VueTextAreaAutosize from 'vue-textarea-autosize'
+import VueTextAreaAutosize from 'vue-textarea-autosize';
 import firebase from 'firebase/app';
-import 'firebase/firestore'
+import 'firebase/firestore';
+import { firebaseConfig } from './config/env';
 
-
-Vue.use(VueTextAreaAutosize)
-Vue.config.productionTip = false
+Vue.use(VueTextAreaAutosize);
+Vue.use(VueComposition);
+Vue.config.productionTip = false;
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCClIKtXcV8d52u62zTmocmevxa9zAPapU",
-  authDomain: "vue-calendar-nxzs.firebaseapp.com",
-  databaseURL: "https://vue-calendar-nxzs.firebaseio.com",
-  projectId: "vue-calendar-nxzs",
-  storageBucket: "vue-calendar-nxzs.appspot.com",
-  messagingSenderId: "409872657722",
-  appId: "1:409872657722:web:0a8e979a89d7c98437b403"
+  ...firebaseConfig,
 });
 
 export const db = firebase.firestore();
 
 new Vue({
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
